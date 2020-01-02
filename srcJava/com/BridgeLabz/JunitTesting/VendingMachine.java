6693ca23e77ad3160632297a8bf5145179607b67
Lakshmi256 <lp268742@gmail.com>
2020-01-02 11:09:51 +0530

    [Lakshmi prasad]add. changes made in vending machine program

--- srcJava/com/BridgeLabz/JunitTesting/VendingMachine.java
+++ srcJava/com/BridgeLabz/JunitTesting/VendingMachine.java
@@ -1,17 +1,19 @@
 package com.BridgeLabz.JunitTesting;
 
 public class VendingMachine {
-	public static void vend(int cash)
+	public static int vend(int cash)
 {
 	int arr[]=new int[] {1000,500,100,50,10,5,2,1};
+	int sum=0;
 	for (int i=0;i<arr.length;i++)
 	{
 	if (cash>=arr[i]);
 	{
 	int x=cash/arr[i];
 	cash=cash%arr[i];
-	System.out.println(arr[i]+" notes:"+x);
+	sum=sum+x;
 }
 	}
+	return sum;
 	}
 }
--- srcTest/com/BridgeLabz/JunitTesting/TestVendingMachine.java
+++ srcTest/com/BridgeLabz/JunitTesting/TestVendingMachine.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 class TestVendingMachine {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testforNumberofNotesVendingMachineReturns() {
+		assertEquals(8,VendingMachine.vend(5512));
+		assertEquals(22,VendingMachine.vend(9999));
+		assertEquals(14,VendingMachine.vend(8765));
+		assertEquals(12,VendingMachine.vend(1799));
+		assertEquals(13,VendingMachine.vend(1996));
 	}
 
 }
