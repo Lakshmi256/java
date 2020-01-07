c19ca588e309124fa47ad22ff64b9064a5fb47ef
Lakshmi256 <lp268742@gmail.com>
2020-01-07 19:03:02 +0530

    [lakshmi prasad]add.Structual programs implemented

---
+++ src/com/BridgeLabz/Facade/MySqlHelper.java
@@ -0,0 +1,19 @@
+package com.BridgeLabz.Facade;
+
+import java.sql.Connection;
+
+public class MySqlHelper {
+
+	public static Connection getMySqlDBConnection(){
+		
+		return null;
+	}
+	
+	public void generateMySqlPDFReport(String tableName, Connection con){
+		
+	}
+	
+	public void generateMySqlHTMLReport(String tableName, Connection con){
+		
+	}
+}
