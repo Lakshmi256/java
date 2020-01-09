cf44d1d164bb8ddf4155c493a0cffe9313f6e854
Lakshmi256 <lp268742@gmail.com>
2020-01-09 11:49:46 +0530

    [lakshmi prasad]add.behavioral design pattren programs implemented

---
+++ src/com/BridgeLabz/Visitor/ItemElement.java
@@ -0,0 +1,5 @@
+package com.BridgeLabz.Visitor;
+
+public interface ItemElement {
+	public int accept(ShoppingCartVisitor visitor);
+}
---
+++ src/com/BridgeLabz/proxyDesign/CommandExecutor.java
@@ -0,0 +1,5 @@
+package com.BridgeLabz.proxyDesign;
+
+public interface CommandExecutor {
+	public void runCommand(String cmd) throws Exception;
+	}
